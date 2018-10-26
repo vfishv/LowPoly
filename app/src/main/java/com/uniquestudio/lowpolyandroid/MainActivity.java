5b17c3ebf899998e63b66f0c3d19c710801d960e
vFISHv <513450035@qq.com>
2018-10-26 19:19:44 +0800

    build

--- app/src/main/java/com/uniquestudio/lowpolyandroid/MainActivity.java
+++ app/src/main/java/com/uniquestudio/lowpolyandroid/MainActivity.java
@@ -4,22 +4,33 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.uniquestudio.lowpoly.LowPoly;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = (ImageView) findViewById(R.id.iv);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gg, options);
-        imageView.setImageBitmap(bitmap);
-        Bitmap out = LowPoly.generate(bitmap, 40);
-        imageView.setImageBitmap(out);
+        imageView = (ImageView) findViewById(R.id.iv);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gg, options);
+                imageView.setImageBitmap(bitmap);
+                Bitmap out = LowPoly.generate(bitmap, 40);
+                if(bitmap!=null && !bitmap.isRecycled()){
+                    bitmap.recycle();
+                }
+                imageView.setImageBitmap(out);
+            }
+        });
+
     }
 }
